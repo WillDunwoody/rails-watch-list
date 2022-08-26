2f7e55bf7e59535fab3a5e90f95d285b200f6007
will dunwoody <will.dunwoody@gmail.com>
2022-08-26 12:02:06 +0200

    fixed root issue

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,27 @@
+class BookmarksController < ApplicationController
+  def new
+    # @list =  '/lists/:list_id/bookmarks/new'
+    @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
+    @movies = Movie.all
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+
+    @params = bookmark_param
+    @bookmark.movie_id = @params[:movie]
+    @bookmark.comment = @params[:comment]
+    @bookmark.list = @list
+    @bookmark.save
+
+    redirect_to list_path(@list)
+  end
+
+  private
+
+  def bookmark_param
+    params.require(:bookmark).permit(:comment, :movie)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get '/', to: 'lists#index'
+  resources :lists, only: [:index, :show, :new, :create] do
+    resource :bookmarks, only: [:new, :create]
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
